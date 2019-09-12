7a1fb2fe48c9ee0cddaab2a1a2a19142a56288a6
Vojtěch Šefler <vojtech_sefler@kb.cz>
2019-09-12 18:39:48 +0200

    first generation generation

--- orchestrator/src/main/java/eu/profinit/hackaton2019/CSOBforehand/CsobForehandApplication.java
+++ orchestrator/src/main/java/eu/profinit/hackaton2019/CSOBforehand/CsobForehandApplication.java
@@ -5,9 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CsobForehandApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(CsobForehandApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CsobForehandApplication.class, args);
+    }
 
 }
---
+++ orchestrator/src/main/java/eu/profinit/hackaton2019/CSOBforehand/services/InitService.java
@@ -0,0 +1,53 @@
+package eu.profinit.hackaton2019.CSOBforehand.services;
+
+import eu.profinit.hackaton2019.CSOBforehand.model.Cell;
+import eu.profinit.hackaton2019.CSOBforehand.model.Position;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Component
+public class InitService implements CommandLineRunner {
+    public List<Cell> generateFirstGen() {
+        int generation = 0;
+        Map<Integer, String> initMap = new HashMap<>();
+
+        initMap.put(0, "0000000000");
+        initMap.put(1, "0000000000");
+        initMap.put(2, "0000000000");
+        initMap.put(3, "0000000000");
+        initMap.put(4, "0000000000");
+        initMap.put(5, "0000000000");
+        initMap.put(6, "0000000000");
+        initMap.put(7, "0000000000");
+        initMap.put(8, "0000000000");
+        initMap.put(9, "0000000000");
+
+        return initMap.entrySet().stream()
+                      .map(entry -> {
+                          return toCellRow(entry.getValue(), entry.getKey(), generation);
+                      })
+                      .flatMap(List::stream)
+                      .collect(Collectors.toList());
+    }
+
+    private List<Cell> toCellRow(String cellRowDefinition, Integer rowNumber, Integer generation) {
+        List<Cell> result = new ArrayList<>();
+
+        for (int i = 0; i < cellRowDefinition.length(); i++) {
+            result.add(new Cell(cellRowDefinition.charAt(i), generation, new Position(i, rowNumber)));
+        }
+
+        return result;
+    }
+
+    @Override
+    public void run(final String... args) {
+        generateFirstGen();
+    }
+}
