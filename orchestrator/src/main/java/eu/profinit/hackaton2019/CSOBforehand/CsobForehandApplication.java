d5c2b4d113292629b5ca7412a541bee8c26b7db9
rubinek10 <rubesluky@gmail.com>
2019-09-12 17:38:56 +0200

    spring boot

---
+++ orchestrator/src/main/java/eu/profinit/hackaton2019/CSOBforehand/CsobForehandApplication.java
@@ -0,0 +1,13 @@
+package eu.profinit.hackaton2019.CSOBforehand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsobForehandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsobForehandApplication.class, args);
+	}
+
+}
---
+++ orchestrator/src/test/java/eu/profinit/hackaton2019/CSOBforehand/CsobForehandApplicationTests.java
@@ -0,0 +1,16 @@
+package eu.profinit.hackaton2019.CSOBforehand;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CsobForehandApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
